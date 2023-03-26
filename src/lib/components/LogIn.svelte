515813a2eafd06451161af282796651000fc6a9b
joeinnes <joe@joeinn.es>
2023-03-26 14:46:51 +0200

    4 files modified, 1 added

---
+++ src/lib/components/LogIn.svelte
@@ -0,0 +1,35 @@
+<script>
+  let username = '';
+  let password = '';
+</script>
+
+<div class="container">
+<label>
+Username
+<input type="text" />
+</label>
+<label>Password
+<input type="password" />
+</label>
+</div>
+
+<button>Submit</button>
+
+<style>
+  .container {
+    @apply w-prose shadow rounded;
+  }
+  button {
+    background-color: var(--color-orange-4);
+    color: var(--color-orange-1);
+    @apply px-2 py-1 rounded transition-colors mt-2;
+  }
+  button:hover {
+    background-color: var(--color-orange-5);
+  }
+  input {
+    background-color: var(--color-orange-1);
+    border: 1px solid var(--color-orange-6);
+    @apply px-2 py-1 rounded block w-full;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
 	import './app.css';
-</script>
+	import '/node_modules/reasonable-colors/reasonable-colors.css';
+	import { db } from '$lib/db/db';
+	import LogIn from '$lib/components/LogIn.svelte'
 
-<slot />
+	let isLoggedIn = null;
+	isLoggedIn = db.authStore.isValid;
+	db.authStore.onChange(() => {
+		isLoggedIn = pb.authStore.isValid;
+	})
+	// I won't bother using a special Log In route, it's not needed.
+</script>
+{#if isLoggedIn === null}
+	Loading...
+{:else if isLoggedIn}
+	Logged In
+	<slot />
+{:else}
+	<LogIn />
+{/if}
+
+<style>
+:global(body) {
+	background-color: var(--color-gray-1);
+}
+</style>
